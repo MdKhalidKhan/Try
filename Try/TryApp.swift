633c87e960006ba4dbf672df6b647a5c20e749b7
Raj <rajkhulna@outlook.com>
2022-03-30 01:12:29 +0600

    Initial Commit

---
+++ Try/TryApp.swift
@@ -0,0 +1,17 @@
+//
+//  TryApp.swift
+//  Try
+//
+//  Created by Khalid Khan on 30/3/22.
+//
+
+import SwiftUI
+
+@main
+struct TryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
